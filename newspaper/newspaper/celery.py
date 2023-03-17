669c345563f8c8d1650e8b160a537b856b52e211
Yagub <tykva430@gmail.com>
2023-03-17 21:30:05 +0400

    add and update some files

--- newspaper/newspaper/celery.py
+++ newspaper/newspaper/celery.py
@@ -1,16 +1,11 @@
 import os
 from celery import Celery
 from celery.schedules import crontab
-from news.tasks import news_send
 
-from celery import shared_task
-from django.core.mail import EmailMultiAlternatives
-from news.models import Author, Category, Post, PostCategory, Comment, UserCategory
-from django.template.loader import render_to_string
-from django.utils import timezone
-import datetime
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newspaper.settings')
+
+
 
 
 app = Celery('newspaper')
@@ -18,10 +13,12 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
 
+
+
 app.conf.beat_schedule = {
     'action_every_monday_8am': {
-        'task': 'news_send',
-        'schedule': crontab(),
+        'task': 'news.tasks.news_send',
+        'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
         'args': (),
     },
 }
